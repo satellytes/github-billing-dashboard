1acc09f9c6d26fb0aaed9491db9984ceb777e4ce
Felix Hofmann <felix.hofmann@satellytes.com>
2021-06-01 17:26:59 +0200

    added start-screen

--- src/components/start-screen/start-screen.tsx
+++ src/components/start-screen/start-screen.tsx
@@ -1,6 +1,22 @@
 import React from "react";
-//import rectangleSVG from "./rectangle.svg";
+import rectangleSVG from "./rectangle.svg";
 import styled from "styled-components";
+
+const backgroundRectangle = (
+  <svg
+    width="312"
+    height="283"
+    viewBox="0 0 312 283"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 4C0 1.79086 1.79086 0 4 0H288.526C290.518 0 292.207 1.46695 292.486 3.44019L311.921 140.94C311.973 141.312 311.973 141.688 311.921 142.06L292.486 279.56C292.207 281.533 290.518 283 288.526 283H4C1.79086 283 0 281.209 0 279V4Z"
+      fill="#7A8FCC"
+      fillOpacity="0.3"
+    />
+  </svg>
+);
 
 const IntroductionSentence = styled.h2`
   font-style: normal;
@@ -25,6 +41,36 @@ const Explanation = styled.p`
   }
 `;
 
+const FlowChart = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const FlowChartRectangle = styled.div`
+  width: 33%;
+  margin-right: 8px;
+`;
+
+const InnerFlowChartDiv = styled.div`
+  position: absolute;
+  width: 33%;
+`;
+
+const FlowChartHeadline = styled.h1`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 110%;
+`;
+
+const FlowChartDescription = styled.p`
+  width: 33%;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 150%;
+`;
+
 export const StartScreen = (): JSX.Element => {
   return (
     <>
@@ -37,6 +83,40 @@ export const StartScreen = (): JSX.Element => {
         Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas
         sed diam eget risus varius blandit sit amet non magna.
       </Explanation>
+      <FlowChart>
+        <FlowChartRectangle>
+          <InnerFlowChartDiv>
+            <FlowChartHeadline>1</FlowChartHeadline>
+            <FlowChartDescription>
+              Integer posuere erat a ante venenatis dapibus posuere velit
+              aliquet.{" "}
+            </FlowChartDescription>
+          </InnerFlowChartDiv>
+          <img src={rectangleSVG} />
+        </FlowChartRectangle>
+
+        <FlowChartRectangle>
+          <InnerFlowChartDiv>
+            <FlowChartHeadline>2</FlowChartHeadline>
+            <FlowChartDescription>
+              Integer posuere erat a ante venenatis dapibus posuere velit
+              aliquet.{" "}
+            </FlowChartDescription>
+          </InnerFlowChartDiv>
+          <img src={rectangleSVG} />
+        </FlowChartRectangle>
+
+        <FlowChartRectangle>
+          <InnerFlowChartDiv>
+            <FlowChartHeadline>3</FlowChartHeadline>
+            <FlowChartDescription>
+              Integer posuere erat a ante venenatis dapibus posuere velit
+              aliquet.{" "}
+            </FlowChartDescription>
+          </InnerFlowChartDiv>
+          <img src={rectangleSVG} />
+        </FlowChartRectangle>
+      </FlowChart>
     </>
   );
 };
