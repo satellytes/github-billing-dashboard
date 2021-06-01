08164db2b5f87439aa0d2e0ba682f79cd93e5f3a
Felix Hofmann <felix.hofmann@satellytes.com>
2021-06-01 07:56:49 +0200

    chart inline styling

--- src/components/billing-chart/billing-bar-chart.tsx
+++ src/components/billing-chart/billing-bar-chart.tsx
@@ -59,21 +59,47 @@ export const BillingBarChart = ({
           groupedBy === "daily" ? entriesGroupedPerDay : entriesGroupedPerWeek
         }
       >
-        <CartesianGrid />
+        <CartesianGrid vertical={false} stroke={"rgba(255, 255, 255, 0.1)"} />
         <XAxis
           dataKey={groupedBy === "daily" ? "day" : "week"}
+          axisLine={false}
+          tickLine={false}
+          tick={{ fill: "white" }}
           tickFormatter={(tick) =>
             Date.parse(tick) ? lightFormat(new Date(tick), "dd.MM.") : tick
           }
           interval="preserveStart"
         />
-        <YAxis domain={[0, maxValueOfYAxis]} unit=" $" />
+        <YAxis
+          domain={[0, maxValueOfYAxis]}
+          unit=" $"
+          tickCount={maxValueOfYAxis + 1}
+          tick={{ fill: "white" }}
+          axisLine={false}
+          tickLine={false}
+        />
         {/*labelFormatter checks if the given label has the right format*/}
         <CustomTooltip
           labelFormatter={(label) =>
             Date.parse(label) ? lightFormat(new Date(label), "dd.MM.") : label
           }
           itemSorter={(item) => (item.value ? item.value * -1 : 0)}
+          labelStyle={{
+            color: "black",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: "12px",
+            lineHeight: "110%",
+            marginBottom: "4px",
+          }}
+          itemStyle={{
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: "12px",
+            lineHeight: "110%",
+          }}
+          contentStyle={{ borderRadius: "4px", borderBlockColor: "white" }}
+          cursor={{ fill: "rgba(122, 143, 204, 0.3)" }}
         />
         <Legend />
         {repositoryNames.map((repositoryName, index) => {
