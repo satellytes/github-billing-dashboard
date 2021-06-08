0f5350b450b3f8b352b2e0bc805bf23b075b1587
Felix Hofmann <felix.hofmann@satellytes.com>
2021-06-08 16:24:42 +0200

    added Coco Gothic font

--- src/global-styles.tsx
+++ src/global-styles.tsx
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from "styled-components";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import CocoGothicRegularTFF from "./fonts/Coco-Gothic-Regular-TTF.ttf";
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: "Coco Gothic Regular";
+    src: url(${CocoGothicRegularTFF}) format("truetype");
+  }
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
+    //font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    //"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    //sans-serif;
+    font-family: "Coco Gothic Regular", Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background: #202840;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import App from "./App";
+import "./fonts/Coco-Gothic-Regular-TTF.ttf";
 
 ReactDOM.render(
   <>
