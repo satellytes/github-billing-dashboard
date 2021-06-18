1ea95905c9f63ee41a43e972d268748e695113a2
Felix Hofmann <felix.hofmann@satellytes.com>
2021-06-18 10:32:57 +0200

    chore: move chart-container.tsx in billing-chart folder

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MonthlyWidgetContainer } from "../components/monthly-widget-container/monthly-widget-container";
-import { ChartContainer } from "../components/chart-container/chart-container";
+import { ChartContainer } from "../components/billing-chart/chart-container";
 import { UsageReportEntry } from "../util/csv-reader";
 import { RepositoryTable } from "../components/repository-table/repository-table";
 import { CurrentTimePeriode } from "../components/headlines/current-time-periode";
