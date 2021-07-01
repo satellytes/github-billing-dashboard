63a5564ddb5dd242f3dfb480a33c303bbaf4ba82
Felix Hofmann <felix.hofmann@satellytes.com>
2021-07-01 09:33:57 +0200

    feat: add Github logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import GithubLogoPNG from "../../assets/GitHub-Mark-Light-64px.png";
 
 const StyledHeader = styled.div`
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
   padding: 20px 24px 14px 24px;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const Logo = styled.h1`
-  margin-left: 24px;
+const SatellytesLogo = styled.h1`
   font-style: normal;
   font-weight: bold;
   font-size: 20px;
   line-height: 31px;
 `;
 
+const GithubLogo = styled.img`
+  width: 32px;
+  cursor: pointer;
+`;
+
 export const Header = (): JSX.Element => {
   return (
     <StyledHeader>
-      <Logo>Satellytes</Logo>
+      <SatellytesLogo>Satellytes</SatellytesLogo>
+      <a
+        href={"https://github.com/satellytes/github-billing-dashboard"}
+        target={"_blank"}
+        rel="noreferrer"
+      >
+        <GithubLogo src={GithubLogoPNG} />
+      </a>
     </StyledHeader>
   );
 };
