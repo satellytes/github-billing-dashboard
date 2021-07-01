c75065f0f2da731f051b4053d0f697a4b21ae555
Felix Hofmann <felix.hofmann@satellytes.com>
2021-07-01 10:31:21 +0200

    feat: add satellytes swoosh

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import GithubLogoPNG from "../../assets/GitHub-Mark-Light-64px.png";
+import { Swoosh } from "../../assets/swoosh";
 
 const StyledHeader = styled.div`
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
   padding: 20px 24px 14px 24px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
-const SatellytesLogo = styled.h1`
+const SatellytesLogo = styled.div``;
+
+const SatellytesLogoText = styled.h1`
   font-style: normal;
   font-weight: bold;
   font-size: 20px;
@@ -21,10 +25,19 @@ const GithubLogo = styled.img`
   cursor: pointer;
 `;
 
+const HeaderSwoosh = styled(Swoosh)`
+  position: absolute;
+  width: 14px;
+  margin-bottom: -7px;
+`;
+
 export const Header = (): JSX.Element => {
   return (
     <StyledHeader>
-      <SatellytesLogo>Satellytes</SatellytesLogo>
+      <SatellytesLogo>
+        <HeaderSwoosh />
+        <SatellytesLogoText>Satellytes</SatellytesLogoText>
+      </SatellytesLogo>
       <a
         href={"https://github.com/satellytes/github-billing-dashboard"}
         target={"_blank"}
--- src/components/headlines/main-headline.tsx
+++ src/components/headlines/main-headline.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { Swoosh } from "../../assets/swoosh";
 import { GridItem, up } from "../grid/grid";
 
-const StyledHeadline = styled.h1`
+const HeadlineText = styled.h1`
   font-style: normal;
   font-weight: bold;
   font-size: 56px;
   line-height: 79px;
-  margin-top: 188px;
+  margin-top: 0;
   margin-bottom: 24px;
   ${() => `
     ${up("md")} {
@@ -16,12 +17,28 @@ const StyledHeadline = styled.h1`
   `};
 `;
 
+const HeadlineSwoosh = styled(Swoosh)`
+  position: relative;
+  width: 36px;
+  margin-top: 188px;
+  margin-left: 4px;
+  margin-bottom: -12px;
+  ${() => `
+    ${up("md")} {
+      width: 48px;
+      margin-bottom: 0;
+    }
+  `};
+`;
+
 export const MainHeadline = (): JSX.Element => {
   return (
     <>
-      {/*TODO: Add satellytes symbol*/}
       <GridItem>
-        <StyledHeadline>GitHub CSV Billing Dashboard</StyledHeadline>
+        <div>
+          <HeadlineSwoosh />
+          <HeadlineText>GitHub CSV Billing Dashboard</HeadlineText>
+        </div>
       </GridItem>
     </>
   );
