422cf21f45855da13ec86fa232b320cae81b4c79
Felix Hofmann <57712895+hofmann-felix@users.noreply.github.com>
2022-10-21 14:53:20 +0200

    feat: show error message if csv has wrong schema (#27)
    * feat: show error message if csv has wrong schema
    
    * chore: update error message

--- src/util/csv-reader.ts
+++ src/util/csv-reader.ts
@@ -21,8 +21,22 @@ const camalize = (str: string): string => {
     .toLowerCase()
     .replace(/[^a-zA-Z0-0]+(.)/g, (m, chr) => chr.toUpperCase());
 };
+
+const hasCorrectCsvSchema = (parseResult: any) => {
+  return (
+    !Object.prototype.hasOwnProperty.call(parseResult, "date") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "product") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "repositorySlug") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "quantity") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "unitType") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "pricePerUnit") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "actionsWorkflow") ||
+    !Object.prototype.hasOwnProperty.call(parseResult, "notes")
+  );
+};
+
 export const getCsvFile = (file: File): Promise<UsageReportEntry[]> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     parse<UsageReportCsvEntry>(file, {
       header: true,
       skipEmptyLines: true,
@@ -30,6 +44,13 @@ export const getCsvFile = (file: File): Promise<UsageReportEntry[]> => {
         return camalize(header);
       },
       complete: (result) => {
+        if (
+          result.errors.length !== 0 ||
+          result.data.find(hasCorrectCsvSchema)
+        ) {
+          reject();
+          return;
+        }
         const githubBillingEntries: UsageReportEntry[] = result.data.map(
           (dailyEntry) => {
             // remove dollar sign
