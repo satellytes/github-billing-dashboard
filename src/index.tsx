f18a2b1e8fd26fabab4ddf45f211a0a54de9f70c
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2023-05-02 09:40:14 +0200

    feat: upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import "./assets/fonts/Coco-Gothic-Regular-TTF.ttf";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
