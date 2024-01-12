c5e1beb102661c2fbf499389ebea68443cf11a39
hieki <ksakahieki@gmail.com>
2024-01-12 19:13:00 +0900

    Use IndexedDB instead of LocalStorage

--- src/util/local-storage.ts
+++ src/util/local-storage.ts
@@ -1,4 +1,5 @@
 import { UsageReportEntry } from "./csv-reader";
+import * as localforage from "localforage"
 
 export interface LocalStorageEntry {
   filename: string;
@@ -7,25 +8,37 @@ export interface LocalStorageEntry {
 
 const LOCAL_STORAGE_ENTRY_KEY_NAME = "GitHubBillingDashboard_";
 
-export const saveFileInLocalStorage = (
+export const saveFileInLocalStorage =  (
   githubBillingEntries: UsageReportEntry[],
   filename: string
 ): void => {
-  localStorage.setItem(
+  localforage.setItem(
     LOCAL_STORAGE_ENTRY_KEY_NAME + filename,
     JSON.stringify({ filename, entries: githubBillingEntries })
-  );
+  ).then((_data) => {
+    console.log("saved")
+  }).catch((err) => {
+    console.error(err)
+  })
 };
 
 export const getBillingFilesFromLocalStorage = (): LocalStorageEntry[] => {
-  const allLocalStorageEntries = Object.entries(localStorage);
-  return allLocalStorageEntries.map((entry) => {
-    if (entry[0].startsWith(LOCAL_STORAGE_ENTRY_KEY_NAME)) {
-      return JSON.parse(entry[1]);
-    }
+  const result: Array<LocalStorageEntry> = [];
+  localforage.keys((allLocalStorageEntries: string[] | null) => {
+    if (!allLocalStorageEntries) return
+    allLocalStorageEntries
+      .filter((entry: string | null) => {
+        return entry?.startsWith(LOCAL_STORAGE_ENTRY_KEY_NAME)
+      })
+      .forEach((entry: string) => {
+        return localforage.getItem(entry, (item) => {
+          result.push(JSON.parse(item))
+        })
+      });
   });
+  return result;
 };
 
-export const removeFileFromLocalStorage = (filename: string): void => {
-  localStorage.removeItem(LOCAL_STORAGE_ENTRY_KEY_NAME + filename);
+export const removeFileFromLocalStorage = async (filename: string): Promise<void> => {
+  await localforage.removeItem(LOCAL_STORAGE_ENTRY_KEY_NAME + filename);
 };
